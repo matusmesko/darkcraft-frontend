bf363f63a01fd37e09735ee07f4b77b25e7a0df0
Matúš <mymail@gmail.com>
2024-08-25 17:16:46 +0200

    Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
       {children}
+      <Footer/>
       </body>
     </html>
   );
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from "./page.module.scss"
+import Wrapper from "../wrapper/Wrapper"
+import Link from "next/link"
+
+const Footer = () => {
+
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer className={styles.footer}>
+            <Wrapper>
+                <div className={styles.container}>
+                    <div className={styles.upperContainer}>
+                        <div className={styles.textContainer}>
+                            <img src="/darkcraftText.png" alt="darkcraft"/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur </p>
+                            <div className={styles.linkContainer}>
+                                <Link href={"/hlasovani"}>HLASOVÁNÍ</Link>
+                                <Link href={"/pravidla"}>PRAVIDLA</Link>
+                            </div>
+                        </div>
+
+                        <div className={styles.imgContainer}>
+                            <img src="/mc.png" alt="img"/>
+                        </div>
+                    </div>
+
+                    <div className={styles.authorContainer}>
+                        <p>©{currentYear} <span>DarkCraft.cz</span> / Všechna práva vyhrazena / Design & created by <a href="http://matusmesko.xyz/" target={"_blank"}>M3SK1</a></p>
+                    </div>
+                </div>
+            </Wrapper>
+        </footer>
+    );
+};
+
+export default Footer;
