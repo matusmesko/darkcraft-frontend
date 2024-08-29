77b33c945f71205321767db1df20644aefa4f632
Matúš <mymail@gmail.com>
2024-08-29 13:58:43 +0200

    Edit

--- src/app/hlasovani/layout.tsx
+++ src/app/hlasovani/layout.tsx
@@ -4,7 +4,7 @@ import SmallHeader from "@/components/smallHeader/SmallHeader";
 export default function Hlasovani({children}: { children: React.ReactNode }) {
     return (
         <>
-            <SmallHeader text={"Hlasování"} desc={"Hlasováním na stránkách zvýšíte dosah našeho serveuru"}/>
+            <SmallHeader text={"Hlasování"} desc={"Hlasováním na stránkách zvýšíte dosah našeho serveru"}/>
             {children}
         </>
     )
